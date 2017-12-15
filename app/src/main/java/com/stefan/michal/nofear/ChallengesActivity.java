10074669359e0744f534238c50aead1c98054977
Michal Dolinski <michalorestes@gmail.com>
2017-12-15 00:06:11 +0000

    app bar: title and action buttons

--- app/src/main/java/com/stefan/michal/nofear/ChallengesActivity.java
+++ app/src/main/java/com/stefan/michal/nofear/ChallengesActivity.java
@@ -5,7 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import com.stefan.michal.nofear.Adapters.ChallengesRecycleViewAdapter;
 
@@ -19,6 +23,8 @@ public class ChallengesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenges);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
         recycleView = findViewById(R.id.recycleView);
         layoutManager = new LinearLayoutManager(this);
@@ -59,5 +65,28 @@ public class ChallengesActivity extends AppCompatActivity {
             }
         });
         recycleView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
